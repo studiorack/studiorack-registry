fd87f5579c041c35e323a3dd2c5bffa5d7873636
Kim T <kimturley@gmail.com>
2020-12-14 21:26:19 -0800

    Use semvar validation for plugin versions

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
+import * as semver from 'semver';
 import slugify from 'slugify';
 import {
   dirCreate,
   fileJsonCreate,
   getJSON,
   Plugin,
-  PluginEntry,
   PluginPack,
   validatePluginSchema,
 } from '@studiorack/core';
@@ -35,43 +35,38 @@ async function getResults(url: string, dir: string, filename: string) {
 }
 
 async function getReleases(result: any) {
-  // this is temporary code to prototype multiple repo types
   const pluginPack: PluginPack = {};
   try {
+    // Get releases for a GitHub repo
     const releases = await getJSON(result.releases_url.replace('{/id}', ''));
     for (const release of releases) {
-      const version = release.tag_name.replace(/[^0-9.]/g, '');
-      // multiple plugins
+      // For each release get plugins.json
       const pluginsJsonList = await getPlugins(
         `https://github.com/${result.full_name}/releases/download/${release.tag_name}/plugins.json`
       );
-      pluginsJsonList.plugins.forEach((pluginItem: Plugin) => {
-        if (pluginItem.id && pluginItem.name && pluginItem.version) {
-          const plugin: PluginEntry = {
-            id: slugify(`${result.full_name}/${pluginItem.id}`, { lower: true, remove: /[^\w\s$*_+~.()'"!\-:@\/]+/g }),
-            version,
+      pluginsJsonList.plugins.forEach((plugin: Plugin) => {
+        // For each plugin sanitize the id and add to registry
+        const pluginId = slugify(`${result.full_name}/${plugin.id}`, { lower: true, remove: /[^\w\s$*_+~.()'"!\-:@\/]+/g });
+        const pluginVersion = semver.coerce(plugin.version)?.version || '0.0.0';
+        console.log('plugin', pluginId, pluginVersion);
+        if (!pluginPack[pluginId]) {
+          pluginPack[pluginId] = {
+            id: pluginId,
+            version: pluginVersion,
             versions: {},
           };
-          plugin.versions[version] = {
-            author: pluginItem.author,
-            homepage: pluginItem.homepage,
-            name: pluginItem.name,
-            description: pluginItem.description,
-            tags: pluginItem.tags,
-            version: pluginItem.version,
-            date: pluginItem.date,
-            size: pluginItem.size,
-          };
-          if (pluginPack[plugin.id]) {
-            pluginPack[plugin.id].versions[version] = plugin.versions[version];
-          } else {
-            pluginPack[plugin.id] = plugin;
-          }
+        }
+        // plugin.release = release.tag_name;
+        pluginPack[pluginId].versions[pluginVersion] = plugin;
+        // If plugin version is greater than the current, set as latest version
+        if (semver.gt(pluginVersion, pluginPack[pluginId].version)) {
+          pluginPack[pluginId].version = pluginVersion;
         }
       });
     }
     return pluginPack;
   } catch (error) {
+    console.log(error);
     return error;
   }
 }
