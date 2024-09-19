05b3677427766a3c3bc4bab7be4d3583d9174697
Kim T <kimturley@gmail.com>
2024-09-18 20:39:20 -0700

    Remove log file and scan directory instead

--- src/validate.ts
+++ src/validate.ts
@@ -1,20 +1,9 @@
-import {
-  apiBuffer,
-  dirCreate,
-  dirExists,
-  fileCreate,
-  fileExists,
-  fileReadString,
-  pathGetExt,
-  PluginFile,
-  PluginFiles,
-} from '@studiorack/core';
+import { apiBuffer, dirCreate, dirExists, fileCreate, pathGetExt, PluginFile, PluginFiles } from '@studiorack/core';
 import { validatePluginYaml } from './sources/local.js';
 import chalk from 'chalk';
 import path from 'path';
 
-const DIR_DOWNLOADS = 'downloads';
-const DIR_LOG = path.join(DIR_DOWNLOADS, 'log.txt');
+const DIR_DOWNLOADS: string = 'downloads';
 
 const filepath: string = process.argv[2];
 const ext: string = pathGetExt(filepath);
@@ -22,11 +11,9 @@ const ext: string = pathGetExt(filepath);
 if (ext === 'yaml') {
   // Ensure directory and log file exist
   if (!dirExists(DIR_DOWNLOADS)) dirCreate(DIR_DOWNLOADS);
-  if (!fileExists(DIR_LOG)) fileCreate(DIR_LOG, '');
 
   // Validate the schema and fields
   const { plugin } = validatePluginYaml(filepath);
-  let log: string = fileReadString(DIR_LOG);
 
   // Download files and compare
   for (const type in plugin.files) {
@@ -39,9 +26,7 @@ if (ext === 'yaml') {
     } else {
       console.log(chalk.red(`X ${pluginFile.url} size needs updating to ${pluginFileBuffer.length}`));
     }
-    // Add file to log for virus scanning
+    // Add file to downloads for virus scanning
     fileCreate(pluginFileLocalPath, pluginFileBuffer);
-    log += pluginFileLocalPath + '\n';
   }
-  fileCreate(DIR_LOG, log);
 }
